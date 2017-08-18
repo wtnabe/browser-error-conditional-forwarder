e8ac2254af030a908f2b33ceb7ebee76201b9b61
wtnabe <wtnabe@gmail.com>
2017-08-18 14:43:56 +0900

    process() を実装
     * shouldIgnore() が true なら ( ignoreFilters() のいずれかが true なら ) スルー で false を status にセット
     * shouldForceForward() が true なら ( forceForwardFilters() のいずれかが true なら ) ignore 如何に関わらず forward して結果を status にセット
     * 適切な forwarder がセットされていない場合もスルーで false を status にセット

--- test/browser_error_conditional_forwarder_test.js
+++ test/browser_error_conditional_forwarder_test.js
@@ -1,5 +1,7 @@
 import assert from 'power-assert'
 import jsdom  from 'jsdom'
+import sinon  from 'sinon'
+
 const { JSDOM } = jsdom
 import BrowserErrorConditionalForwarder, {
   BrowserErrorAbstractForwarder,
@@ -11,6 +13,31 @@ class BadFilterWithoutFilterMethod {}
 class TestingAlwayFalseFilter {
   filter(message, source, lineno, colno, error = undefined) {
     return false
+  }
+}
+
+class forwardCatcher {
+  /** @var {Object} */
+  captured
+
+  /**
+   * @param {String}  message
+   * @param {String}  source
+   * @param {Integer} lineno
+   * @param {Integer} colno
+   * @param {Object}  error
+   * @return {Boolean}
+   */
+  forward(message, source, lineno, colno, error = undefined) {
+    this.captured = {
+      message: message,
+      source:  source,
+      lineno,  lineno,
+      colno:   colno,
+      error:   error
+    }
+
+    return true
   }
 }
 
@@ -142,6 +169,72 @@ describe('BrowserErrorConditionalForwarder', ()=> {
 
       it('return true', ()=> {
         assert.equal(true, subject())
+      })
+    })
+  })
+
+  describe('#process', ()=> {
+    function fireSimpleError(opts = undefined) {
+      window.dispatchEvent(new window.ErrorEvent('error', opts))
+    }
+
+    describe('has valid forwarder', ()=> {
+      beforeEach(()=> {
+        forwarder.forwarder(forwardCatcher)
+      })
+
+      describe('should ignore', ()=> {
+        beforeEach(()=> {
+          sinon.stub(forwarder, 'shouldIgnore').returns(true)
+          fireSimpleError()
+        })
+
+        it('status false', ()=> {
+          assert.equal(false, forwarder.forwardStatus())
+        })
+      })
+
+      describe('should ignore and should force forward', ()=> {
+        beforeEach(()=> {
+          sinon.stub(forwarder, 'shouldIgnore').returns(true)
+          sinon.stub(forwarder, 'shouldForceForward').returns(true)
+          fireSimpleError({
+            message: 'simple error',
+            lineno:  0,
+            colno:   0,
+            error:   {}
+          })
+        })
+
+        it('status true', ()=> {
+          assert.equal(true, forwarder.forwardStatus())
+        })
+
+        it('forwarded error', ()=> {
+          assert.deepEqual(
+            {
+              message: 'simple error',
+              source:  '',
+              lineno:  0,
+              colno:   0,
+              error:   {}
+            },
+            forwarder.forwarder().captured)
+        })
+      })
+    })
+
+    describe('no valid forwarder', ()=> {
+      beforeEach(()=> {
+        fireSimpleError()
+      })
+
+      it('forwarder is undefined', ()=> {
+        assert.equal(undefined, forwarder.forwarder())
+      })
+
+      it('status false ( not undefined )', ()=> {
+        assert.equal(false, forwarder.forwardStatus())
       })
     })
   })
